2499be9bae8c75b8c694d7c876a77623cc096528
Adela Tullio <adela.tullio@gmail.com>
2022-03-08 12:01:37 -0500

    review model and repository

--- src/main/java/models/Review.java
+++ src/main/java/models/Review.java
@@ -14,17 +14,19 @@ public class Review {
     private Product reviewedProduct;
 
     private int reviewRating;
-
+    private String reviewComment;
     /**
      * Constructor to initialize a new review for a product
-     * @param author
-     * @param product
-     * @param rating
+     * @param author to be initialized to
+     * @param product to be initialized to
+     * @param rating to be initialized to
+     * @param comment to be initialized to
      */
-    public Review(User author, Product product, int rating) {
+    public Review(User author, Product product, int rating, String comment) {
         this.reviewAuthor = author;
         this.reviewedProduct = product;
         this.reviewRating = rating;
+        this.reviewComment = comment;
     }
 
     /**
@@ -43,7 +45,7 @@ public class Review {
 
     /**
      * Setting the review id to a specified id
-     * @param id
+     * @param id id to be set to
      */
     public void setId(Long id) {
         this.id = id;
@@ -51,7 +53,7 @@ public class Review {
 
     /**
      * Setting author to a specified user
-     * @param user
+     * @param user user to be set to
      */
     public void setReviewAuthor(User user) {
         this.reviewAuthor = user;
@@ -67,7 +69,7 @@ public class Review {
 
     /**
      * Setting the product that was reviewed
-     * @param product
+     * @param product product to be set to
      */
     public void setReviewedProduct(Product product) {
         this.reviewedProduct = product;
@@ -83,7 +85,7 @@ public class Review {
 
     /**
      * Setting the rating for the product review
-     * @param rating
+     * @param rating rating to be set to
      */
     public void setReviewRating(int rating) {
         this.reviewRating = rating;
@@ -95,5 +97,21 @@ public class Review {
      */
     public int getReviewRating() {
         return this.reviewRating;
+    }
+
+    /**
+     * Setting review comments
+     * @param comment comments for product review
+     */
+    public void setReviewComment(String comment) {
+        this.reviewComment = comment;
+    }
+
+    /**
+     * Getting comments for review
+     * @return comment for the specified review
+     */
+    public String getReviewComment() {
+        return this.reviewComment;
     }
 }
--- src/main/java/repositories/ReviewRepository.java
+++ src/main/java/repositories/ReviewRepository.java
@@ -1,4 +1,7 @@
 package repositories;
 
-public class ReviewRepository {
+import models.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
 }
