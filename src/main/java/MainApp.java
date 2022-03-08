37884704ebae3750fdd084175fe7e80c9c90980f
Liya Abebe <43866253+liyaabebe@users.noreply.github.com>
2022-03-07 23:58:26 -0500

    initial project setup

---
+++ src/main/java/MainApp.java
@@ -0,0 +1,8 @@
+import org.springframework.boot.SpringApplication;
+
+public class MainApp {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApp.class, args);
+    }
+
+}
