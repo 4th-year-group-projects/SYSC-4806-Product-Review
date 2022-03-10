22fcfbe05df851cf48960cbbb3f15e3b60491905
noahmank <39310260+noahmank@users.noreply.github.com>
2022-03-10 12:38:24 -0500

    Initial implementation of view, model, controller, and repository

---
+++ src/main/java/application/models/Product.java
@@ -0,0 +1,79 @@
+package application.models;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue
+    private long Id;
+
+//    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+//    private List<Review> reviews;
+    private String name, description, link;
+
+    public Product(long Id, String name, String description, String link) {
+        this.Id = Id;
+        //this.reviews = new ArrayList<Review>();
+        this.name = name;
+        this.description = description;
+        this.link = link;
+    }
+
+    public Product(String name, String description, String link) {
+        //this.reviews = new ArrayList<Review>();
+        this.name = name;
+        this.description = description;
+        this.link = link;
+    }
+
+    public Product() {}
+
+    public long getId() {
+        return this.Id;
+    }
+
+    public void setId(Long id) {
+        this.Id = id;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+    public String getName() {
+        return this.name;
+    }
+
+    public String getLink() {
+        return this.link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+//    @Transient
+//    public void addReview(Review review) {
+//        this.reviews.add(review);
+//    }
+//
+//    @Transient
+//    public void removeReview()
+    @Override
+    @Transient
+    public String toString() {
+        return "{" +
+                this.Id + ", " +
+                this.name + ", " +
+                this.description + ", " +
+                this.link + "}";
+    }
+}
