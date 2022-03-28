d7209fcfc5ed6248b669b4b51826dc4976801669
noahmank <39310260+noahmank@users.noreply.github.com>
2022-03-28 13:10:50 -0400

    Improve category enum

--- src/main/java/application/Category.java
+++ src/main/java/application/Category.java
@@ -1,5 +1,8 @@
 package application;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public enum Category {
     TECH("Electronics, Computers, & Office"),
     CLOTHING("Apparel"),
@@ -8,13 +11,30 @@ public enum Category {
     BOOKS("Books"),
     SPORTS("Sports & Outdoors");
 
-    private final String categoryName;
+    private static final Map<String, Category> BY_LABEL = new HashMap<>();
 
-    private Category(String categoryName) {
-        this.categoryName = categoryName;
+    static {
+        for (Category c: values()) {
+            BY_LABEL.put(c.label, c);
+        }
     }
 
-    public String getCategoryName() {
-        return this.categoryName;
+    private final String label;
+
+    private Category(String label) {
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return this.label;
+    }
+
+    public static Category valueOfLabel(String label) {
+        return BY_LABEL.get(label);
+    }
+
+    @Override
+    public String toString() {
+        return this.label;
     }
 }
